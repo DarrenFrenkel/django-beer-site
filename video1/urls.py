e3719e1901dd999291105afdb79b3ff7e7130c55
Darren Frenkel <darrenfrenkel@gmail.com>
2014-02-03 23:25:54 +0000

    updated files

--- video1/urls.py
+++ video1/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,4 +12,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 	(r'^beers/$', 'beer.views.BeersAll' ),
+)
+
+urlpatterns += patterns('',
+    url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),	   
 )
