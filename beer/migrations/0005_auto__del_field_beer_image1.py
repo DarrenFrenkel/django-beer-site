85c199da716649b97b9a67d5fac83821684b7579
Darren Frenkel <darrenfrenkel@gmail.com>
2014-02-11 08:44:59 +0000

    updated beer model

---
+++ beer/migrations/0005_auto__del_field_beer_image1.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Beer.image1'
+        db.delete_column(u'beer_beer', 'image1')
+
+
+    def backwards(self, orm):
+        # Adding field 'Beer.image1'
+        db.add_column(u'beer_beer', 'image1',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True),
+                      keep_default=False)
+
+
+    models = {
+        u'beer.beer': {
+            'Meta': {'object_name': 'Beer'},
+            'brewery': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'beers'", 'to': u"orm['beer.Brewery']"}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'localilty': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        u'beer.brewery': {
+            'Meta': {'object_name': 'Brewery'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['beer']
